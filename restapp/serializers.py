c1f857557b1da2765e6444a2ad61674f5f722fbe
Amit <amit@tonara.com>
2020-08-20 17:48:51 +0530

    Revert "tweak"
    This reverts commit 86cee8dd

--- restapp/serializers.py
+++ restapp/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
